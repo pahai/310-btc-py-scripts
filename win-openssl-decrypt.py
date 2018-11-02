fdf1eccdc869fbd74db4cccf365b5556d12b8eb8
pahai <34192974+pahai@users.noreply.github.com>
2018-11-02 20:22:57 +0800

    Create win-openssl-decrypt.py

---
+++ win-openssl-decrypt.py
@@ -0,0 +1,25 @@
+import itertools
+import subprocess
+
+filename =r'C:\ENCRYPTED.txt'
+
+sets = [ ['L3','3L'], ['02','20'], ['485','584'], ['9F','F9'], ['7'] ]
+all_item_permutations = []
+
+sets_permutations = list(itertools.permutations(sets, len(sets)))
+
+for sets_permutation in sets_permutations:
+    item_permutations = list(itertools.product(*sets_permutation))
+    all_item_permutations += item_permutations
+#print(all_item_permutations)
+
+keys = []
+for permutation in all_item_permutations:
+    keys.append(''.join(permutation))
+#print(keys)
+
+for key in keys:
+	proc =  subprocess.Popen([r'C:\OpenSSL-Win64\bin\openssl','enc','-aes-256-cbc','-md', 'md5', '-base64','-d','-a','-k', key, '-in', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out,err = proc.communicate()
+	if 'bad decrypt' not in str(err):
+		print(out,key)
